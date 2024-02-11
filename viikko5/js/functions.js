2f314ed3c5ee4186db5ce3ca76e0cccb633bad9a
JaLuigi <t2luja00@students.oamk.fi>
2024-02-11 15:07:49 +0200

    viikko5

--- viikko5/js/functions.js
+++ viikko5/js/functions.js
@@ -1,5 +1,5 @@
-function rollDice() {
-    var diceImage = document.getElementById("dice").getElementsByTagName("img")[0];
-    const random_number = Math.floor(Math.random() * 6) + 1;
-    diceImage.src = "img/" + random_number + ".png";
-}
+document.getElementById('dice').addEventListener('click', function() {
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    var imgElement = document.querySelector('#dice img');
+    imgElement.src = './img/' + randomNumber + '.png';
+});
